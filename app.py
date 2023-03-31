e29c90bdbe0a5153969a95adf94948077e0eb7d3
anorprogrammer <anorprogrammer1127@gmail.com>
2023-03-31 17:53:07 +0500

    config file added

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import logging
 from aiogram import Bot, Dispatcher, executor, types
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
-API_TOKEN = 'BOT TOKEN HERE'
+from config import BOT_TOKEN
 
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token=API_TOKEN)
+bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 scheduler = AsyncIOScheduler()
 
